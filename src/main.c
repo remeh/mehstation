c6df0035257618b0d85ce9f52897c82d067fe772
remeh <me@remy.io>
2015-02-08 21:56:50 +0100

    Window added.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "SDL2/SDL.h"
+#include "glib-2.0/glib.h"
+
+#include "view/window.h"
 
 int main(int argc, char* argv[]) {
-	SDL_Init(SDL_INIT_HAPTIC);
-	SDL_Quit();
+	// Nearly everything is used in the SDL.
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		g_critical("Can't init the SDL: %s", SDL_GetError());
+		return 1;
+	}
+	g_message("SDL init OK.");
+
+	const Window* window = meh_create_window(640, 480, FALSE);
+
+	SDL_Delay(1000);
+
+	atexit(SDL_Quit);
 	return 0;
 }
