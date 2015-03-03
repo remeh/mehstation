fccadbd914c05076d7369d450149f7ff3c69d3a6
remeh <remeh@remeh.fr>
2015-03-03 07:54:00 +0100

    Update the schema.

--- res/schema.sql
+++ res/schema.sql
@@ -1,16 +1,30 @@
+BEGIN TRANSACTION;
 CREATE TABLE `platform`  (
 	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	`name`	TEXT NOT NULL,
 	`command`	TEXT NOT NULL
 );
-CREATE TABLE `executable` (
-	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`display_name`	TEXT NOT NULL,
-	`filepath`	TEXT NOT NULL,
-    `platform_id` INTEGER NOT NULL
-);
 CREATE TABLE `mehstation` (
     `name` TEXT NOT NULL PRIMARY KEY UNIQUE,
     `value` TEXT
 );
-INSERT INTO mehstation VALUES ('schema', '1');
+CREATE TABLE "executable_resource" (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`executable_id`	INTEGER NOT NULL,
+	`type`	TEXT NOT NULL,
+	`filepath`	TEXT NOT NULL
+);
+CREATE TABLE "executable" (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`display_name`	TEXT NOT NULL,
+	`filepath`	TEXT NOT NULL,
+	`platform_id`	INTEGER NOT NULL,
+	`description`	TEXT,
+	`genre`	TEXT,
+	`developer`	TEXT,
+	`release_date`	TEXT,
+	`publisher`	TEXT,
+	`players`	TEXT,
+	`rating`	TEXT
+);
+INSERT INTO `mehstation` VALUES ('schema','1');
