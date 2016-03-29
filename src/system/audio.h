d6b2d989c893743cfdc72148e835d6535c80974f
remeh <remeh@remeh.fr>
2016-03-29 22:30:57 +0200

    Audio file loading.

--- src/system/audio.h
+++ src/system/audio.h
@@ -6,7 +6,27 @@
 
 #pragma once
 
+#include <glib-2.0/glib.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+
 typedef struct {
+	gchar* filename;
+
+	/* ffmpeg part */
+
+	AVFormatContext* fc;
+	/* id of the stream in the format context */
+	int stream_id;
+
+	/* codec context inside the stream */
+	AVCodecContext* stream_codec_ctx;
+	/* codec context we've created to use */
+	AVCodecContext* codec_ctx;
+
+	AVCodec* codec;
+
+	AVFrame* frame;
 } Sound;
 
 Sound* meh_sound_new();
