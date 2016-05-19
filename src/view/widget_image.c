4026cb54b2e2f236b604554f40332ad8cec5a410
remeh <remeh@remeh.fr>
2016-05-19 13:43:59 +0200

    widget_image: method to compute aspect ratio of the image.

--- src/view/widget_image.c
+++ src/view/widget_image.c
@@ -55,11 +55,50 @@ void meh_widget_image_render(Window* window, const WidgetImage* image) {
 
 	// Convert the normalized position to the window position.
 	SDL_Rect rect = {
-		meh_window_convert_width(window, image->x.value),
-		meh_window_convert_height(window, image->y.value),
-		meh_window_convert_width(window, image->w.value),
-		meh_window_convert_height(window, image->h.value)
+		meh_window_convert_width(window, image->x.value + image->offset_x),
+		meh_window_convert_height(window, image->y.value + image->offset_y),
+		meh_window_convert_width(window, image->display_w), /* TODO must be transition */
+		meh_window_convert_height(window, image->display_h)
 	};
 
 	meh_window_render_texture(window, image->texture, NULL, &rect);
+}
+
+void meh_widget_image_compute_aspect_ratio(WidgetImage* image) {
+	g_assert(image != NULL);
+
+	if (image->texture == NULL) {
+		g_critical("meh_widget_image_compute_aspect_ratio called with NULL texture.");
+		return;
+	}
+
+	SDL_QueryTexture(image->texture, NULL, NULL, &image->tex_w, &image->tex_h);
+
+	/* compute the size */
+
+	image->display_w = image->tex_w;
+	image->display_h = image->tex_h;
+
+	if (image->tex_w > image->w.value) {
+		image->display_w = image->w.value;
+		image->display_h = (image->w.value / (float)image->tex_w) * (float)image->tex_h;
+	}
+
+	/* TODO(remy): what if h > tex_h ? */
+
+	/* compute the position inside the target rect */
+
+	image->offset_x = 0;
+	if (image->w.value - image->display_w > 0.01) {
+		image->offset_x = (image->w.value - image->display_w)/2;
+	}
+
+	g_debug("offset_x: %d", image->offset_x);
+
+	image->offset_y = 0;
+	if (image->h.value - image->display_h > 0.01) {
+		image->offset_y = (image->h.value - image->display_h)/2;
+	}
+
+	g_debug("offset_y: %d", image->offset_y);
 }
--- src/view/widget_image.h
+++ src/view/widget_image.h
@@ -23,8 +23,19 @@ typedef struct {
 
 	/* On which texture this widget is pointing. Do not free this pointer. */
 	SDL_Texture* texture;
+
+	/* size of the internal texture */
+	int tex_w;
+	int tex_h;
+
+	/* where is display on the screen the image, after ratio computation */
+	int offset_x;
+	int offset_y;
+	int display_w;
+	int display_h;
 } WidgetImage;
 
 WidgetImage* meh_widget_image_new(SDL_Texture* texture, float x, float y, float w, float h);
 void meh_widget_image_destroy(WidgetImage* image);
 void meh_widget_image_render(Window* window, const WidgetImage* image);
+void meh_widget_image_compute_aspect_ratio(WidgetImage* image);
